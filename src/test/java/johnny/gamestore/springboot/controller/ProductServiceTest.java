a92b79c03a22b33766ff115ffec70da31f93d57d
jojozhuang <jojozhuang@gmail.com>
2022-01-18 21:11:06 -0800

    add integration test

--- src/test/java/johnny/gamestore/springboot/controller/ProductServiceTest.java
+++ src/test/java/johnny/gamestore/springboot/controller/ProductServiceTest.java
@@ -6,8 +6,6 @@ import johnny.gamestore.springboot.repository.ProductRepository;
 import johnny.gamestore.springboot.service.ProductService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
