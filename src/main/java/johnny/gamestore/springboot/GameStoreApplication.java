a38ab4df02f6b0573d59216bd4c5aa892703168c
jojozhuang <jojozhuang@gmail.com>
2022-01-22 09:59:43 -0800

    add checkstyle dependency

--- src/main/java/johnny/gamestore/springboot/GameStoreApplication.java
+++ src/main/java/johnny/gamestore/springboot/GameStoreApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GameStoreApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(GameStoreApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(GameStoreApplication.class, args);
+  }
 }
