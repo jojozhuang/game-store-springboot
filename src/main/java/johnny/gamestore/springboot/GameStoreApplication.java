aa6a1c058f149cd1a2bc12c8f8f700b85f0b1f7d
jojozhuang <jojozhuang@gmail.com>
2022-01-22 19:57:04 -0800

    fix all style errors.

--- src/main/java/johnny/gamestore/springboot/GameStoreApplication.java
+++ src/main/java/johnny/gamestore/springboot/GameStoreApplication.java
@@ -3,6 +3,9 @@ package johnny.gamestore.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main entry of the application.
+ */
 @SpringBootApplication
 public class GameStoreApplication {
   public static void main(String[] args) {
