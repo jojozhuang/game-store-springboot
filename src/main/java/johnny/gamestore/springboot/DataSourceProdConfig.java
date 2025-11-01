e4c121594a4dacb9f2c11e53dcaaf7c3e8d4cb9e
Rong Zhuang <jojozhuang@gmail.com>
2025-10-31 22:50:33 -0700

    Fix sonar issue in datasource config (#28)
    * fix sonar issue in datasource config
    
    * fix issue in render config
    
    * Use slf4j to write log

--- src/main/java/johnny/gamestore/springboot/DataSourceProdConfig.java
+++ src/main/java/johnny/gamestore/springboot/DataSourceProdConfig.java
@@ -1,5 +1,6 @@
 package johnny.gamestore.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,26 +8,33 @@ import org.springframework.context.annotation.Profile;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+@Slf4j
 @Configuration
-public class DataSourceConfig {
+public class DataSourceProdConfig {
+  private static final int USERINFO_SPLIT_LIMIT = 2;
+
   @Profile("prod")
   @Bean
   public BasicDataSource dataSource() throws URISyntaxException {
     String databaseUrl = System.getenv("DATABASE_URL");
+    log.info("DATABASE_URL: {}", databaseUrl);
     System.out.println("DATABASE_URL:" + databaseUrl);
+    if (databaseUrl == null || databaseUrl.isEmpty()) {
+      throw new IllegalStateException("DATABASE_URL environment variable is not set.");
+    }
     // DATABASE_URL sample: postgres://<username>:<password>@<host>:<port>/<dbname>
     URI dbUri = new URI(databaseUrl);
-
-    String dbUrl =
-        "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
     if ("localhost".equalsIgnoreCase(dbUri.getHost())) {
       dbUrl += "?sslmode=disable";
     } else {
       dbUrl += "?sslmode=require";
     }
-    String username = dbUri.getUserInfo().split(":")[0];
-    String password = dbUri.getUserInfo().split(":")[1];
 
+    String[] userInfoParts = dbUri.getUserInfo() != null
+      ? dbUri.getUserInfo().split(":", USERINFO_SPLIT_LIMIT) : new String[0];
+    String username = userInfoParts.length > 0 ? userInfoParts[0] : "";
+    String password = userInfoParts.length > 1 ? userInfoParts[1] : "";
     BasicDataSource basicDataSource = new BasicDataSource();
     basicDataSource.setUrl(dbUrl);
     basicDataSource.setUsername(username);
--- src/main/java/johnny/gamestore/springboot/DataSourceRenderConfig.java
+++ src/main/java/johnny/gamestore/springboot/DataSourceRenderConfig.java
@@ -1,5 +1,6 @@
 package johnny.gamestore.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,19 +9,27 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collections;
 
+@Slf4j
 @Configuration
-public class DataSourceConfigRender {
+public class DataSourceRenderConfig {
+  private static final int USERINFO_SPLIT_LIMIT = 2;
+
   @Profile("render")
   @Bean
   public BasicDataSource dataSource() throws URISyntaxException {
     String databaseUrl = System.getenv("DATABASE_URL");
-    System.out.println("DATABASE_URL:" + databaseUrl);
+    log.info("DATABASE_URL: {}", databaseUrl);
+    if (databaseUrl == null || databaseUrl.isEmpty()) {
+      throw new IllegalStateException("DATABASE_URL environment variable is not set.");
+    }
     // DATABASE_URL sample: postgres://<username>:<password>@<host>:<port>/<dbname>
     URI dbUri = new URI(databaseUrl);
-
     String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-    String username = dbUri.getUserInfo().split(":")[0];
-    String password = dbUri.getUserInfo().split(":")[1];
+
+    String[] userInfoParts = dbUri.getUserInfo() != null
+      ? dbUri.getUserInfo().split(":", USERINFO_SPLIT_LIMIT) : new String[0];
+    String username = userInfoParts.length > 0 ? userInfoParts[0] : "";
+    String password = userInfoParts.length > 1 ? userInfoParts[1] : "";
 
     BasicDataSource basicDataSource = new BasicDataSource();
     basicDataSource.setUrl(dbUrl);
