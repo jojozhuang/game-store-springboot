4e2ad88792095c681c979c7776e0be509bdd0d59
Rong Zhuang <jojozhuang@gmail.com>
2025-11-02 09:18:39 -0800

    Improve test coverage (#32)
    * add test for application and base controller
    
    * change back to private

---
+++ src/test/java/johnny/gamestore/springboot/GameStoreApplicationTest.java
@@ -0,0 +1,12 @@
+package johnny.gamestore.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreApplicationTest {
+  @Test
+  void contextLoads() {
+    // If the application context fails to start, this test will fail
+  }
+}
